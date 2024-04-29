497aab2b8373bc494cd1cf029ef72b48ef97151a
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-04-29 18:06:29 +0200

    create unorder-list

--- src/app/TestBranch.js
+++ src/app/TestBranch.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function TestBranch() {
    return (
-      <div>TestBranch</div>
+      <div>TestBranch
+         <h2>TestBranch</h2>
+         <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+         </ul>
+      </div>
    )
 }
 
