61649fbd2540d750f1a6f20d39e60d82875ba5d3
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-04-29 18:15:57 +0200

    add Header.js -  main header

---
+++ src/app/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+   return (
+      <div className='w-full bg-red-200 p-4 font-bold text-xl text-center'>
+         <h1>Main Header of project</h1>
+      </div>
+   )
+}
+
+export default Header
