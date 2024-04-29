f0a6a970e8d944f86315504b3f360652fde19146
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-04-29 19:14:45 +0200

    add menu

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Menu from './Menu'
 
 function Header() {
    return (
-      <div className='w-full bg-red-200 p-4 font-bold text-xl text-center'>
+      <header className='w-full bg-red-200 p-4 font-bold text-xl text-center'>
+         <Menu />
          <h1>Main Header of project</h1>
-      </div>
+      </header>
    )
 }
 
