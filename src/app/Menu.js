62013bb15502e516fd0b5d48a90d2d487ddf0a86
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-04-29 19:15:56 +0200

    add menu

---
+++ src/app/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Menu() {
+   return (
+      <nav>
+         <a href="#">Home</a>
+         <a href="#">Menu</a>
+         <a href="#">About</a>
+      </nav>
+   )
+}
+
+export default Menu
