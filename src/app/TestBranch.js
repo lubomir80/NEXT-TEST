e7c2a3f744a8e7009c2ddf0313972a7f6444d064
Lubomir <100048089+lubomir80@users.noreply.github.com>
2024-04-29 17:59:05 +0200

    TestBranch element

---
+++ src/app/TestBranch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TestBranch() {
+   return (
+      <div>TestBranch</div>
+   )
+}
+
+export default TestBranch
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import TestBranch from "./TestBranch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <html lang="en">
          <body className={inter.className}>
             {children}
+            <TestBranch />
             <footer className="bg-red-200">TEST</footer>
          </body>
       </html>
